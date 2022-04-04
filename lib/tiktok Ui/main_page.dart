f35a2fbd131a2c47c8d119b2eb1a92f32d0c9105
ZahangirAlamJP <thezahangir@gmail.com>
2022-04-04 12:28:37 +0600

    bottomNavigationBar - Bottom Color Selected

--- lib/tiktok Ui/main_page.dart
+++ lib/tiktok Ui/main_page.dart
@@ -1,24 +1,35 @@
 import 'package:flutter/material.dart';
 
-class MainPage extends StatelessWidget {
+class MainPage extends StatefulWidget {
   const MainPage({ Key? key }) : super(key: key);
 
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+
+  int selectedIndex = 0;
+  void GotoPages(int index) {
+    setState(() {
+      selectedIndex = index;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: Colors.white,
-        unselectedItemColor: Colors.black,
+        backgroundColor: Colors.black,
+        unselectedItemColor: Colors.amber,
         type: BottomNavigationBarType.fixed,
-        
-     
-        
+        currentIndex: selectedIndex, 
+        onTap: GotoPages,
         items: [
         BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
         BottomNavigationBarItem(icon: Icon(Icons.search), label: 'search'),
         BottomNavigationBarItem(icon: Icon(Icons.add_box_rounded), label: ''),
         BottomNavigationBarItem(icon: Icon(Icons.chat_bubble), label: 'Chat'),
-        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+        BottomNavigationBarItem(icon: Icon(Icons.person), label: 'User'),
          
         ],
        ),
