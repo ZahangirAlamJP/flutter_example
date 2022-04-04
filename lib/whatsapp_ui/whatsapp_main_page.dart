4d2213760ef4bd760d990a92a40d1d2c195a37d5
ZahangirAlamJP <thezahangir@gmail.com>
2022-04-04 10:58:10 +0600

    DefaultTabController TabBar

--- lib/whatsapp_ui/whatsapp_main_page.dart
+++ lib/whatsapp_ui/whatsapp_main_page.dart
@@ -10,20 +10,43 @@ class WhatsappMainPage extends StatefulWidget {
 class _WhatsappMainPageState extends State<WhatsappMainPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color(0xff075e55),
-        title: Text('Whatsapp'),
-        actions: [
-          PopupMenuButton(itemBuilder: (_) {
-            return [
-              PopupMenuItem(child: Text("setting")),
-               PopupMenuItem(child: Text("setting")),
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color(0xff075e55),
+          title: Text('Whatsapp'),
+          actions: [
+            PopupMenuButton(itemBuilder: (context) {
+              return [
                 PopupMenuItem(child: Text("setting")),
                  PopupMenuItem(child: Text("setting")),
-            ];
-          })
-        ],
+                  PopupMenuItem(child: Text("setting")),
+                   PopupMenuItem(child: Text("setting")),
+              ];
+            }),
+          ],
+          bottom: TabBar(
+            indicatorColor: Colors.amber,
+            tabs: [
+            Tab(
+              text: "Camaera",
+              icon: Icon(Icons.camera),
+            ),
+            Tab(
+              text: "Chat",
+              icon: Icon(Icons.camera),
+            ),
+            Tab(
+              text: "Status",
+              icon: Icon(Icons.camera),
+            ),
+            Tab(
+              text: "Calls",
+              icon: Icon(Icons.camera),
+            ),
+          ]),
+        ),
       ),
     );
   }
